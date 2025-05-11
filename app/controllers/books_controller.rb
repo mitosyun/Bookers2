bda561dcaf53685eabfe207f10944120d1d65d2d
EC2 Default User <ec2-user@ip-172-31-8-132.ap-northeast-1.compute.internal>
2025-05-11 12:17:29 +0000

    課題_62

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,27 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
     @booknew = Book.new
-    @book.user = current_user
+    @book.user == current_user
   end
 
   def edit
@@ -23,8 +30,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit  
+    end
   end
 
   def destroy
@@ -36,6 +47,13 @@ class BooksController < ApplicationController
   private
   def book_params
     params.require(:book).permit(:title, :body, :image)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
 
 end
